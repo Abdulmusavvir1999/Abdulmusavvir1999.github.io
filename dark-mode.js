9e957925a9168180e590762d67dfbd77b0894ef3
Abdulmusavvir1999 <abdulmusavir8888@gmail.com>
2024-03-15 15:04:09 +0530

    first commit

---
+++ dark-mode.js
@@ -0,0 +1,33 @@
+// dark-mode
+
+var app = document.getElementsByTagName("BODY")[0];
+
+const darkMode = document.querySelector(".light-mode-button")
+
+darkMode.addEventListener("click", () => {
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.lightMode == "dark") {
+
+    localStorage.lightMode = "light";
+    app.setAttribute("light-mode", "light");
+  } else {
+
+    localStorage.lightMode = "dark";
+    app.setAttribute("light-mode", "dark");
+  }
+
+})
+
+window.onload = () => {
+
+  if (localStorage.lightMode == "dark") {
+    app.setAttribute("light-mode", "dark");
+  } 
+  else {
+    app.setAttribute("light-mode", "light");
+  }
+}
+
+if (localStorage.lightMode == "dark") {
+  app.setAttribute("light-mode", "dark");
+}
---
+++ script.js
@@ -0,0 +1,40 @@
+const scrollDown = document.querySelector(".scroll-down")
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 150) {
+        scrollDown.classList.add("active");
+    } else {
+        scrollDown.classList.remove("active");
+    }
+})
+
+
+// image sliders
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
