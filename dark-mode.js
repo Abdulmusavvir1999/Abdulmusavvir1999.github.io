bbe68c0957f2b8cee64a94efbe13e8c7289273e6
Abdulmusavvir1999 <abdulmusavir8888@gmail.com>
2024-08-19 11:48:01 +0530

    add own img some style

--- dark-mode.js
+++ dark-mode.js
@@ -2,36 +2,9 @@
 
 var app = document.getElementsByTagName("BODY")[0];
 
-const darkMode = document.querySelector(".light-mode-button")
+const darkMode = document.querySelector(".night-toggle")
 
-// darkMode.addEventListener("click", () => {
-//   var app = document.getElementsByTagName("BODY")[0];
-//   if (localStorage.lightMode == "dark" || false) {
-
-//     localStorage.lightMode = "light";
-//     app.setAttribute("light-mode", "light");
-//   } else {
-
-//     localStorage.lightMode = "dark";
-//     app.setAttribute("light-mode", "dark");
-//   }
-
-// })
-
-window.onload = () => {
-
-  if (localStorage.lightMode == "dark" || false) {
-    app.setAttribute("light-mode", "dark");
-  }
-  else {
-    app.setAttribute("light-mode", "light");
-  }
-}
-
-if (localStorage.lightMode == "dark") {
-  app.setAttribute("light-mode", "dark");
-}
-function switchMode() {
+darkMode.addEventListener("click", () => {
   var app = document.getElementsByTagName("BODY")[0];
   if (localStorage.lightMode == "dark" || false) {
 
@@ -42,16 +15,49 @@ function switchMode() {
     localStorage.lightMode = "dark";
     app.setAttribute("light-mode", "dark");
   }
+
   let moon = document.getElementById("moon");
   if (moon.className == "moon") {
     moon.className = "sun";
-    document.body.style.backgroundColor = "#141D26";
-    document.body.style.color = "#fff";
   }
   else {
     moon.className = "moon";
-    document.body.style.backgroundColor = "#fff";
-    document.body.style.color = "#000";
   }
+
+})
+
+
+
+if (localStorage.lightMode == "dark") {
+  app.setAttribute("light-mode", "dark");
 }
+// function switchMode() {
+//   let moon = document.getElementById("moon");
+//   if (moon.className == "moon") {
+//     moon.className = "sun";
+//     document.body.style.backgroundColor = "#141D26";
+//     document.body.style.color = "#fff";
+//   }
+//   else {
+//     moon.className = "moon";
+//     document.body.style.backgroundColor = "#fff";
+//     document.body.style.color = "#000";
+//   }
+
+// }
+window.onload = () => {
+
+  if (localStorage.lightMode == "dark" || false) {
+    app.setAttribute("light-mode", "dark");
+    moon.className = "sun";
+
+  }
+  else {
+    app.setAttribute("light-mode", "light");
+    moon.className = "moon";
+
+  }
+
+}
+
 
